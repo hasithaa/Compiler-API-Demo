ae87906838827c5065ea4aee48158a358604e8fb
Hasitha Aravinda <hasithaa@users.noreply.github.com>
2023-01-27 15:25:58 +0530

    Add Syntax Tree and Semantic Model based runtime annotation analysis

--- src/main/java/tips/bal/examples/compiler/AnnotationAnalyser.java
+++ src/main/java/tips/bal/examples/compiler/AnnotationAnalyser.java
@@ -1,19 +1,10 @@
 /*
- *  Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
+ * MIT License
  *
- *  WSO2 LLC. licenses this file to you under the Apache License,
- *  Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License.
- *  You may obtain a copy of the License at
+ * Copyright (c) 2023 Hasitha Aravinda. All rights reserved.
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing,
- *  software distributed under the License is distributed on an
- *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  KIND, either express or implied.  See the License for the
- *  specific language governing permissions and limitations
- *  under the License.
+ * This work is licensed under the terms of the MIT license.
+ * For a copy, see <https://opensource.org/licenses/MIT>.
  */
 package tips.bal.examples.compiler;
 
